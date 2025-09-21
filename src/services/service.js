546fa9fbb07601e25b2a3c1000da049ff60d5830
Efrat Friedman <efratfriedman18@gmail.com>
2025-09-21 14:41:57 +0300

    build caching mechanism

--- src/services/service.js
+++ src/services/service.js
@@ -1,5 +1,16 @@
+const STORAGE_KEY="products_cache";
+
 export const getProducts = async () => {
-    return fetch('https://fakestoreapi.com/products')
-        .then(response => response.json())
-        .then(data => console.log(data));
+    const cached=localStorage.getItem(STORAGE_KEY);
+    if(cached){
+        console.log("returning from cache");
+        return JSON.parse(cached);
+        
+    }
+    const response=await fetch('https://fakestoreapi.com/products')
+     const data=await response.json();
+
+     localStorage.setItem(STORAGE_KEY,JSON.stringify(data))
+
+     return data;
 };
