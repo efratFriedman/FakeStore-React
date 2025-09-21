bedfcd85f1d41f58dbd18a39e2cb31df07570224
Efrat Friedman <efratfriedman18@gmail.com>
2025-09-21 15:46:18 +0300

    cards of items

--- src/StoreItems.jsx
+++ src/StoreItems.jsx
@@ -1,23 +1,31 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { getProducts } from "./services/service";
+import './StoreItems.css';
 
 const StoreItems = () => {
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        getProducts().then((products) => {
-            setItems(products);
-        });
-    }, []);
+  useEffect(() => {
+    getProducts().then((products) => {
+      setItems(products);
+    });
+  }, []);
 
+  return (
+    <>
+    <h1>Our products</h1>
+    <div className="store-container">
+      {items.map((item) => (
+        <div className="store-card" key={item.id}>
+          <img src={item.image} alt={item.title} className="store-image" />
+          <h3 className="store-title">{item.title}</h3>
+          <p className="store-description">{item.description}</p>
+          <p className="store-price">${item.price}</p>
+        </div>
+      ))}
+    </div>
+    </>
+  );
+};
 
-    useEffect(() => {
-        console.log("Items changed:", items);
-    }, [items]);
-    return (
-        <></>
-
-    )
-}
-
-export default StoreItems
+export default StoreItems;
